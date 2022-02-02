5184e268d11ab1d58412c7a2159d660b0071e4c3
Luis Barrios <luisfbarrios2021@gmail.com>
2022-02-02 10:31:48 -0500

    actualizar producto

--- ecommerce/src/main/java/com/boecommerce/ecommercecontroller/ProductoController.java
+++ ecommerce/src/main/java/com/boecommerce/ecommercecontroller/ProductoController.java
@@ -1,6 +1,8 @@
 package com.boecommerce.ecommercecontroller;
 
 
+import java.util.Optional;
+
 import com.boecommerce.ecommercemodel.Producto;
 import com.boecommerce.ecommercemodel.Usuario;
 import com.boecommerce.ecommerceservice.ProductoService;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +42,23 @@ public class ProductoController {
         producto.setUsuario(u);
         productoService.save(producto);
         return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+
     }
     
     
