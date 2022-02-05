c364ad2426dc9ce31a225d03f8a3732abf2e4af1
Luis Barrios <luisfbarrios2021@gmail.com>
2022-02-04 19:48:32 -0500

    redireccionando al carrito de compras

--- ecommerce/src/main/java/com/boecommerce/ecommercecontroller/HomeController.java
+++ ecommerce/src/main/java/com/boecommerce/ecommercecontroller/HomeController.java
@@ -1,7 +1,11 @@
 package com.boecommerce.ecommercecontroller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import com.boecommerce.ecommercemodel.DetalleOrden;
+import com.boecommerce.ecommercemodel.Orden;
 import com.boecommerce.ecommercemodel.Producto;
 import com.boecommerce.ecommerceservice.ProductoService;
 
@@ -10,7 +14,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -18,6 +24,12 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+
+    //Almacenar detalle de orden
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    //datos de la orden
+    Orden orden = new Orden();
 
     @GetMapping("")
     public String home (Model model){
@@ -38,4 +50,15 @@ public class HomeController {
         return "usuario/productohome";
     }
     
+    @PostMapping("/cart")
+    public String addCart (@RequestParam Integer id, @RequestParam Integer cantidad  ){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+        
+
+        return "usuario/carrito.html";
+    }
 }
